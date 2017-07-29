7c3237d7ea30b2ff10d7fede59bf5aa86777fc20
jvt057 <vigneshwaran.kennady@capitalone.com>
2017-07-29 16:19:46 -0400

    initial commit

--- example/Example.js
+++ example/Example.js
@@ -8,6 +8,11 @@ const datetime = require('node-datetime');
 
 const express = require('express');
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 /*app.use(bodyParser.json());
 app.use(bodyParser.json({
 	type : 'application/vnd.api+json'
